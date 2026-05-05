79a7604594230bbb84742cc5dc8168e996b6bda4
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2026-05-04 21:42:52 -0500

    Importamos las rutas y la forma en que nos conectamos a ellas

--- pqr_backend/src/app.js
+++ pqr_backend/src/app.js
@@ -1,10 +1,13 @@
 // En este archivo lo que haremos es hacer las configuraciones necesarias para la aplicación, como va a funcionar nuestra API.
 import express from 'express';
+import usersRoutes  from './routes/users.routes.js'
 
 // Inicializamos, creando una instancia y así configurar, crear rutas, hacer solicitudes.
  const app = express();
 
  // Middleware para entender JSON.
  app.use(express.json());
+ // 👇 aquí conectamos las rutas
+app.use(usersRoutes);
 
  export default app;
