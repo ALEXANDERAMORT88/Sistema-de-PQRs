13eaa83eec245587d0095c0b50ac9e7c82ab7bcc
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2026-06-03 21:28:40 -0500

    Se crean rutas GET para consultas de PQR

--- pqr_backend/src/routes/pqr.routes.js
+++ pqr_backend/src/routes/pqr.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getPqr, createPqr } from "../controllers/pqr.controllers.js";
+import { getPqr, getPqrId, getPqrByRadicado, createPqr } from "../controllers/pqr.controllers.js";
 
 const router = Router();
 
-router.get('/pqrs', getPqr); // BUsqueda de todas PQRs creadas.
-router.post('/pqrs', createPqr )// Crear una nueva PQRs.
+router.get('/pqrs', getPqr); // Búsqueda de todas PQR creadas.
+router.get('/pqrs/:id', getPqrId); // Búsqueda de PQR según su ID, configuración estará creada para administradores y backend.
+router.get('/pqrs/radicado/:radicado', getPqrByRadicado); // Búsqueda de PQR con el número de radicado, configuración creada para usuarios.
+router.post('/pqrs', createPqr ); // Crear una nueva PQR.
 
 export default router;
