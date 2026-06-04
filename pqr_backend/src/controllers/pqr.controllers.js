00c766a2bf330024a17cc9e20d8fd0d58c477a27
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2026-06-03 21:22:27 -0500

    Se configura el controlador PQR para búsqueda por ID o número de radicado

--- pqr_backend/src/controllers/pqr.controllers.js
+++ pqr_backend/src/controllers/pqr.controllers.js
@@ -1,6 +1,6 @@
 import { Pqr } from "../models/pqr.models.js";
 
-// Llamar todas las PQR
+// Buscar todas las PQR
 export const getPqr = async (req, res) => {
   try {
     const pqrs = await Pqr.findAll();
@@ -10,14 +10,47 @@ export const getPqr = async (req, res) => {
   }
 };
 
+// Buscar cada PQR con su ID, configuración para administradores y backend.
+export const getPqrId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const pqrId = await Pqr.findByPk(id);
+
+    if (!pqrId) {
+      return status(404).json({ message: "PQR no encontrada ❌" });
+    }
+
+    res.json(pqrId);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Buscar PQR con número de radicado, configuración para usuarios.
+export const getPqrByRadicado = async (req, res) => {
+  try {
+    const { radicado } = req.params;
+
+    const pqr = await Pqr.findOne({
+      where: {
+        numero_radicado: radicado,
+      },
+    });
+
+    if (!pqr) {
+      return res.status(500).json({ message: "PQR no encontrada" });
+    }
+    res.json(pqr);
+    
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Crear una nueva PQR.
 export const createPqr = async (req, res) => {
   const numeroRadicado = `PQR-${Date.now()}`;
-  const {
-    descripcion,
-    fecha_creacion,
-    estado,
-    fecha_vencimiento,
-  } = req.body;
+  const { descripcion, fecha_creacion, estado, fecha_vencimiento } = req.body;
   try {
     const newPqrs = await Pqr.create({
       numero_radicado: numeroRadicado,
@@ -26,7 +59,7 @@ export const createPqr = async (req, res) => {
       estado,
       fecha_vencimiento,
     });
-    res.json(newPqrs)
+    res.json(newPqrs);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
